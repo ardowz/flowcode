5bfbcb1bf48a4f708485265ddd272bd6ce92ba20
bavancena <ardowz@gmail.com>
2018-02-03 16:47:13 -0800

    Adding basic stuff to get slim started

--- src/routes.php
+++ src/routes.php
@@ -5,10 +5,32 @@ use Slim\Http\Response;
 
 // Routes
 
-$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+$app->get("/flowTranslate", function (Request $request, Response $response, array $args) {
     // Sample log message
-    $this->logger->info("Slim-Skeleton '/' route");
+//    $this->logger->info("Slim-Skeleton '/' route");
+
+    //how to get params
+//    $myvar1 = $request->getParam('myvar'); //checks both _GET and _POST [NOT PSR-7 Compliant]
+//    $myvar2 = $request->getParsedBody()['myvar']; //checks _POST  [IS PSR-7 compliant]
+//    $myvar3 = $request->getQueryParams()['myvar']; //checks _GET [IS PSR-7 compliant]
+    // Render index view
+    return $this->renderer->render($response, 'index.phtml', $args);
+});
+
+$app->post("/flowTranslate", function (Request $request, Response $response, array $args) {
+    // Sample log message
+//    $this->logger->info("Slim-Skeleton '/' route");
+    var_dump($request->getParam("hello"));
+    exit();
 
     // Render index view
     return $this->renderer->render($response, 'index.phtml', $args);
 });
+
+//$app->get('/[{name}]', function (Request $request, Response $response, array $args) {
+//    // Sample log message
+//    $this->logger->info("Slim-Skeleton '/' route");
+//
+//    // Render index view
+//    return $this->renderer->render($response, 'index.phtml', $args);
+//});
